5d0e6861fea54f9065e44c23d5bb23c2abb491e0
Anthony <a82moreau@gmail.com>
2019-06-30 17:09:41 +0200

    version0.2

--- classSkillsLoading.js
+++ classSkillsLoading.js
@@ -1,56 +1,69 @@
 class randomSkillsLoading{
 
-    constructor(container,language, min, max, divisionSecond){
+    constructor(container,language, min, max, divisionSecond, pourcentage, random, transition){
 
         this.container = container;
         this.divisionSecond = divisionSecond;
 
         let count = 0;
-        const pourcentage = 100;
 
         let Mycontainer = this.selectC_(this.container);
         let load = this.createDivWithClass('load');
-        let p = document.createElement('p');
+        let p = document.createElement('span');
         Mycontainer.appendChild(load);
         load.appendChild(p);
-        let skills = this.createDivWithClass('skills');
+        let skills = this.createDivWithClass('skills_');
         Mycontainer.appendChild(skills);
-        let skill = this.createDivWithClass('skill');
+        let skill = this.createDivWithClass('skill_');
         skills.appendChild(skill);
 
-        const c = this.selectC_(this.container + ' .load p');
-        const s = this.selectC_(this.container + ' .skill');
+        const c = this.selectC_(this.container + ' .load span');
+        const s = this.selectC_(this.container + ' .skill_');
 
         function getRandom(mn, mx) {
             return (Math.random() * (mx - mn)) - mx
         }
         let i = setInterval(function () {
-            let r = Math.round(Math.random() * getRandom(min, max));
-            count -= r;
+            if(random === 'true'){
+                let r = Math.round(Math.random() * getRandom(min, max));
+                count -= r;
+            } else {
+                count++
+            }
             if(count >= pourcentage){
                 count = pourcentage;
                 clearInterval(i)
+            }if(count === pourcentage){
+                c.innerHTML = count + '%';
+            } else {
+                c.innerHTML = language + ' ' + count + "%";
             }
-            c.innerHTML = language + ' ' + count + "%";
             s.style.width = count + "%";
+            if(transition != null){
+                s.style.transition = "width "+ transition +"s linear"
+            }
+
 
         }, 1000/this.divisionSecond);
 
     }
-
     createDivWithClass(maClass){
         let p = document.createElement('div');
         p.setAttribute('class', maClass);
         return p
-
     }
     selectC_(maClass){
         return document.querySelector(maClass);
     }
-
 }
-
-let skill1 = new randomSkillsLoading('.skill1', 'Loading', 0, 4, 12);
-let skill2 = new randomSkillsLoading('.skill2', 'Chargement', 0, 4, 12);
-let skill3 = new randomSkillsLoading('.skill3', 'Wouah !!', 0, 1.3, 60);
+//example 
+let HTML = new randomSkillsLoading(
+    '.skill1',
+    'Loading',
+    0,
+    4,
+    50 / vitesse,
+    50,
+    'false',
+    transition),
 
